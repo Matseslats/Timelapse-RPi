32ccc023ce778afea7020a524969771cdcc6ec22
Mats <49249061+Matseslats@users.noreply.github.com>
2023-11-02 01:16:09 +0000

    Made progress bar pretty. Added complete message

--- make_timelapse.py
+++ make_timelapse.py
@@ -1,6 +1,11 @@
+import math
 import cv2
 import os
 import time
+from colorama import init
+init(autoreset=True)
+from colorama import Cursor
+from termcolor import colored
 
 # Input folder containing the images
 input_folder = "./downloaded-images-outside/cam-out/"
@@ -9,9 +14,12 @@ input_folder = "./downloaded-images-outside/cam-out/"
 output_video = "timelapse-outside.mp4"
 frame_rate = 60  # Adjust as needed
 frame_size = (3280, 2464)  # Adjust as needed
+use_every_nth_frame = 5
 
 # List all image files in the input folder
 image_files = [f for f in os.listdir(input_folder) if f.lower().endswith(('.png', '.jpg', '.jpeg', '.gif'))]
+image_files = image_files[::use_every_nth_frame]
+
 
 print(f"Found {len(image_files)} files")
 # Sort the image files by their names (assuming the names represent the order)
@@ -23,24 +31,44 @@ out = cv2.VideoWriter(output_video, fourcc, frame_rate, frame_size)
 
 # Loop through the image files and add them to the video
 time_start = time.time()
+display_width = 83
+print_every_nth_frame = math.ceil(min(50, len(image_files)/(display_width-4)))
 for index, image_file in enumerate(image_files):
-    if ((index % 50 == 0 or index == 10) and index >= 1):
-        print("="*33, end="")
-        print(f" {index:7d}/{len(image_files):7d} ", end="")
-        print("="*33)
+    if ((index % print_every_nth_frame == 0 or index == 1) and index >= 1):
+        print("="*int((display_width)//2 -11), end="")
+        print(f" {index:^10d}/{len(image_files):^10d} ", end="")
+        print("="*int((display_width)//2 -11))
         current_time = time.time()
         time_passed = current_time - time_start # Seconds passed
         fraction_complete = index/len(image_files) # 0-1
-        time_left = time_passed/fraction_complete
-        print(f"  Process has taken {time_passed&60:8.1f}m. Estimated {time_left:8.1f}s ({time_left/60:8.1f}min) ({time_left/3600:8.1f}h) left")
-        print("="*83)
-        print()
+        time_left = time_passed/fraction_complete - time_passed
+        print(f"  Process has taken ", 
+              colored(f"{time_passed/60:8.1f}m", 'cyan'),
+              f". Estimated {time_left:8.1f}s (",
+              colored(f"{time_left/60:8.1f}min", 'green'),
+              f") ({time_left/3600:8.1f}h) left", sep="")
         
+        # Prog-bar
+        print(" [", end="")
+        bar_width = display_width-4
+        print(colored("#", "yellow")*math.ceil(bar_width*fraction_complete), end="")
+        print(colored(" ", "white")*math.floor(bar_width*(1-fraction_complete)), end="")
+        print("] ")
+        print("="*display_width)
+        print(Cursor.UP(5))
 
     image_path = os.path.join(input_folder, image_file)
     frame = cv2.imread(image_path)
     out.write(frame)
 
+
+print()
+print()
+print()
+print()
+print()
+
+print(f"Process took ", colored(f"{time_passed/60:8.1f}m", 'cyan'),)
 # Release the video writer
 out.release()
 
